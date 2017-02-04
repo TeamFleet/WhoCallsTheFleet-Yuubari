b02a9cfbc06c0e05a9acf08ec480f7a710ab4bd4
Diablohu <diablohudream@gmail.com>
2017-02-04 19:11:59 +0800

    WIP

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,38 @@ import './components/_base.less'
 
 class Root extends Component {
     componentWillMount() {
-        // get all background images
-        
+        // path
+        const getPath = (dir) => {
+            if (self.nw) {
+                const path = require('path')
+                const mkdirp = require('mkdirp');
+                let result = path.join(self.nw.root, dir)
+                if (dir == 'bgimgs') {
+                    const fs = require('fs')
+                    try {
+                        fs.accessSync(result, fs.F_OK);
+                    } catch (e) {
+                        result = path.join(self.nw.root, '/app/assets/images/homebg/')
+                    }
+                }
+                mkdirp.sync(result)
+                return result
+            } else {
+                if (dir.substr(0, 1) !== '/') dir = '/' + dir
+                return dir
+            }
+        }
+        self.path = {
+            'db': getPath('/app-db/'),
+            'bgimgs': getPath('/bgimgs/'),
+            'bgimgs_custom': getPath('/app/assets/images/homebg-custom/'),
+            'pics': {
+                ships: getPath('/pics-ships/'),
+                shipsExtra: getPath('/pics-ships-extra/'),
+                items: getPath('/pics/items/')
+            }
+        }
+        // KC.path.pics = _g.path.pics
 
         // after all end, remove .loading from body
     }
