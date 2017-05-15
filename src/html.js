2f59c381f96e9fdfb6bc30f739af71b5ff15651b
Diablohu <diablohudream@gmail.com>
2017-05-16 02:33:12 +0800

    minor fix

--- src/html.js
+++ src/html.js
@@ -18,7 +18,7 @@ export const template = `
 
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
         <link rel="icon" href="/icon-128.png" type="image/png" sizes="128x128">
-        <link rel="icon" href="/icon-256.png" type="image/png" sizes="256x256">
+        <!--<link rel="icon" href="/icon-256.png" type="image/png" sizes="256x256">-->
         <script>//inject_manifest</script>
 
         <meta name="theme-color" content="#263238" />
