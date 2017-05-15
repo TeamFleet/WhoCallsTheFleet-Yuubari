2e3ca443a7f7647839389e918e41e38421259ab9
Diablohu <diablohudream@gmail.com>
2017-05-15 18:02:50 +0800

    base infos for ship-details

--- src/client/ui/pages/ship-details.jsx
+++ src/client/ui/pages/ship-details.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router'
 
 import translate from 'sp-i18n'
 import PageContainer from 'sp-ui-pagecontainer'
@@ -28,14 +29,81 @@ export default class extends React.Component {
         return this._data || {}
     }
 
+    renderSeries() {
+        const serieses = this.data._series
+        console.log(serieses)
+        return (
+            <div>
+                <table>
+                    <tbody>
+                        {serieses.map((series, i) => (
+                            <tr key={i}>
+                                <th style={{
+                                    textAlign: 'right'
+                                }}><i>
+                                    {i == 0 && "-"}
+                                    {i > 0 && serieses[i-1].next_lvl}
+                                </i></th>
+                                <td>
+                                    {this.data.id === series.id && db.ships[series.id]._name}
+                                    {this.data.id !== series.id && <Link to={`/ships/${series.id}`}>
+                                        {db.ships[series.id]._name}
+                                    </Link>}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+
+    renderSlotEquipments() {
+        let i = 0
+        let renderArr = []
+        while(i < 4){
+            const slot = this.data.slot[i]
+            const equipmentId = typeof slot !== 'undefined' ? this.data.equip[i] : undefined
+            renderArr.push(
+                <tr key={i}>
+                    <th style={{
+                        textAlign: 'right'
+                    }}><i>{typeof slot !== 'undefined' ? slot : "-"}</i></th>
+                    <td>
+                        {equipmentId &&
+                            <Link to={`/equipments/${equipmentId}`}>
+                                {db.equipments[equipmentId]._name}
+                            </Link>
+                        }
+                        {!equipmentId && "-"}
+                    </td>
+                </tr>
+            )
+            i++
+        }
+        return (
+            <div>
+                <h4>{translate('ship_details.slot_and_equipments')}</h4>
+                <table>
+                    <tbody>
+                        {renderArr}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+
     render() {
-        if (__CLIENT__) console.log('thisShip', this.data)
+        if (__CLIENT__ && __DEV__) console.log('thisShip', this.data)
         return (
             <PageContainer
                 className={this.props.className}
             >
                 <h2>{this.data._name}</h2>
                 <p><i>{translate('under_construction')}...</i></p>
+
+                {this.renderSeries()}
+                {this.renderSlotEquipments()}
             </PageContainer>
         )
     }
