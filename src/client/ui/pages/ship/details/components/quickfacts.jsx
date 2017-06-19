b5bff9b0e0845d50d84024d80953b197c102e8e4
Diablohu <diablohudream@gmail.com>
2017-06-19 17:45:44 +0800

    ship details

--- src/client/ui/pages/ship/details/components/quickfacts.jsx
+++ src/client/ui/pages/ship/details/components/quickfacts.jsx
@@ -13,9 +13,14 @@ import styles from './quickfacts.less'
 @ImportStyle(styles)
 export default class ShipDetailsComponentSlotEquipments extends React.Component {
     render() {
+        const ship = this.props.ship
         return (
             <ComponentContainer className={this.props.className} title={translate("ship_details.quickfacts")}>
-                [PH] QUICK FACTS
+                [{ship.navy.toUpperCase()}] {ship._navyName}
+                {" | "}
+                [{ship.rels.cv}] {ship._cv}
+                {" | "}
+                [{ship.rels.illustrator}] {ship._illustrator}
             </ComponentContainer>
         )
     }
