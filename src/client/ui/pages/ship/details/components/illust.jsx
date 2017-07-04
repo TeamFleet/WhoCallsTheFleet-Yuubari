56f4d7081bdb5b2898846a370187a1cefa768c77
Diablohu <diablohudream@gmail.com>
2017-07-04 18:07:07 +0800

    ship details

--- src/client/ui/pages/ship/details/components/illust.jsx
+++ src/client/ui/pages/ship/details/components/illust.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+// import swiper from 'swiper'
 
 import ComponentContainer from '../commons/component-container.jsx'
 
+import getPic from 'Utils/get-pic.js'
 import translate from 'sp-i18n'
 
 import { ImportStyle } from 'sp-css-import'
@@ -10,10 +12,24 @@ import styles from './illust.less'
 // @connect()
 @ImportStyle(styles)
 export default class ShipDetailsComponentSlotEquipments extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.pics = []
+        const ids = ['_'].concat(props.ship._extraIllust)
+        const illustIds = [8, 9]
+        ids.forEach(id => {
+            illustIds.forEach(illustId => {
+                this.pics.push(
+                    id === '_' ? getPic(props.ship, illustId) : getPic('ship-extra', id, illustId)
+                )
+            })
+        })
+    }
     render() {
         return (
             <ComponentContainer className={this.props.className}>
-                ILLUSTRATIONS
+                {this.pics.map((url, index) => <img key={index} src={url} />)}
             </ComponentContainer>
         )
     }
