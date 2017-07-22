7258c7def0a7790e766e60436f4b9b58212d709c
Diablohu <diablohu@users.noreply.github.com>
2017-07-23 06:00:28 +0800

    ship details: aaci

--- src/client/ui/pages/ship/details/components/aaci.jsx
+++ src/client/ui/pages/ship/details/components/aaci.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import classNames from 'classnames'
 import checkAACI from 'kckit/src/check/aaci'
 
 import ComponentContainer from '../commons/component-container.jsx'
 import IconEquipment from 'UI/components/icon-equipment'
+import Icon from 'UI/components/icon'
 
 import translate from 'sp-i18n'
 
@@ -14,8 +16,15 @@ import styles from './aaci.less'
 export default class ShipDetailsAACI extends React.Component {
     renderAACI(aaci, index) {
         return (
-            <dl className="item" key={index}>
-                <dt className="id">#{aaci.id}</dt>
+            <dl className={classNames(['item', {
+                'is-limited': aaci.ship.isID || aaci.ship.isClass || aaci.ship.isType || aaci.ship.isBB
+            }])} key={index}>
+                <dt className="id">
+                    #{aaci.id}
+                    {aaci.ship.isID && <small>{translate("ship_details.aaci_req.ship")}</small>}
+                    {aaci.ship.isClass && <small>{translate("ship_details.aaci_req.class")}</small>}
+                    {(aaci.ship.isType || aaci.ship.isBB) && <small>{translate("ship_details.aaci_req.type")}</small>}
+                </dt>
                 <dd className="icons">
                     {aaci.icons.map((icon, index) => <IconEquipment key={index} icon={icon} />)}
                 </dd>
@@ -29,15 +38,22 @@ export default class ShipDetailsAACI extends React.Component {
         )
     }
     render() {
+        const aaciTypes = checkAACI(this.props.ship.id)
+        const ableToAACI = (Array.isArray(aaciTypes) && aaciTypes.length) ? true : false
+        if (__DEV__) console.log('thisShip > AACI', aaciTypes)
         return (
             <ComponentContainer className={this.props.className} title={translate("ship_details.aaci")}>
-                <dl className="item header">
+                {!ableToAACI && <div className="unable disabled">
+                    <Icon className="icon" icon="cross" />
+                    {translate("ship_details.aaci_unable")}
+                </div>}
+                {ableToAACI && <dl className="item header">
                     <dt className="id" />
                     <dd className="icons" />
                     <dd className="fixed">{translate("aaci.fixed")}</dd>
                     <dd className="modifier">{translate("aaci.modifier")}</dd>
-                </dl>
-                {checkAACI(this.props.ship.id).map(this.renderAACI.bind(this))}
+                </dl>}
+                {ableToAACI && aaciTypes.map(this.renderAACI.bind(this))}
             </ComponentContainer>
         )
     }
