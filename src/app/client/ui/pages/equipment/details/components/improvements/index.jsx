ad5fdad69689b58c8c3ca30be6043500a9de22db
Diablohu <diablohudream@gmail.com>
2017-08-29 19:30:11 +0800

    wip

--- src/app/client/ui/pages/equipment/details/components/improvements/index.jsx
+++ src/app/client/ui/pages/equipment/details/components/improvements/index.jsx
@@ -11,10 +11,27 @@ import translate from 'sp-i18n'
 // @ImportStyle(styles)
 export default class EquipmentDetailsComponentImprovements extends React.Component {
     render() {
+        const list = this.props.equipment.improvement || []
+        const hasItem = !!(list.length)
         return (
             <ComponentContainer className={this.props.className} title={translate("equipment_details.improvements")}>
-                {translate('under_construction')}
+                {hasItem && list.map((data, index) => (
+                    <EquipmentDetailsComponentImprovementsImprovement data={data} key={index} />
+                ))}
+                {!hasItem && <span className="disabled">{translate("none")}</span>}
             </ComponentContainer>
+        )
+    }
+}
+
+class EquipmentDetailsComponentImprovementsImprovement extends React.Component {
+    render() {
+        const { className, data } = this.props
+        const upgrade = data[0]
+        return (
+            <div className={className}>
+                {JSON.stringify(this.props.data)}
+            </div>
         )
     }
 }
