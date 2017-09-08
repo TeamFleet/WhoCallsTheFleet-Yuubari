eea7619b9aa0a489b0f9c6747e5cb4a6a50f57ba
Diablohu <diablohu@users.noreply.github.com>
2017-09-09 05:59:24 +0800

    deploy will not install devDependencies

--- deploy.js
+++ deploy.js
@@ -41,7 +41,7 @@ const run = () => {
     //     return true
     // })
 
-    runScript('npm install --no-save', '[NPM] install')
+    runScript('npm install --no-save --only=production', '[NPM] install')
         // .then(() => runScript('npm run build', '[NPM] build'))
         // .then(() => runScript('pm2 delete sp-boilerplate', '[PM2] kill service'))
         .then(() => runScript('npm run start:pm2', '[NPM] build & [PM2] starting server'))
