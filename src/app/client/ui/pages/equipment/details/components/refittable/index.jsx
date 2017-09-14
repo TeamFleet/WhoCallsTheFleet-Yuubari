0b8bd88756baade8e96a60414b8ce7e3f240ead5
Diablohu <diablohudream@gmail.com>
2017-09-14 18:45:57 +0800

    [equipment details] reffitable

--- src/app/client/ui/pages/equipment/details/components/refittable/index.jsx
+++ src/app/client/ui/pages/equipment/details/components/refittable/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import classNames from 'classnames'
 
 import ComponentContainer from '@appUI/containers/infos-component'
+import ListShips from '@appUI/components/list/ships'
+import times from '@appUtils/times'
 
 import { db } from 'kckit'
 const {
@@ -61,6 +63,7 @@ export default class EquipmentDetailsComponentRefittable extends React.Component
     }
 }
 
+@ImportStyle(require('./styles-shipcollection.less'))
 class ShipCollection extends React.Component {
     render() {
         const {
@@ -78,13 +81,21 @@ class ShipCollection extends React.Component {
         const types = data.list.map(thisType => {
             const id = thisType.type
             if (!id) return
-            if (cachedTypes.includes(id)) return
-            cachedTypes.push(id)
 
             thisType.ships.forEach(series => {
                 series.forEach(ship => {
-                    if (availableExtraShips.includes(ship.id))
+                    if ((
+                        ship.type_display && ship.type_display !== ship.type
+                        && availableShipTypes.includes(ship.type)
+                        && !availableShipTypes.includes(ship.type_display)
+                    ) || (
+                            availableExtraShips.includes(ship.id)
+                            && !availableShipTypes.includes(ship.type)
+                            && !availableShipTypes.includes(ship.type_display)
+                        )
+                    ) {
                         extraShips.push(ship)
+                    }
                 })
             })
 
@@ -93,11 +104,24 @@ class ShipCollection extends React.Component {
                 code: shipTypes[id].code,
                 on: availableShipTypes.includes(id)
             }
-        }).filter(obj => obj)
+        }).filter(obj => {
+            if (!obj) return false
+            if (cachedTypes.includes(obj.name)) return false
+            cachedTypes.push(obj.name)
+            return true
+        })
+
+        const placeholders = []
+        times(10)(index => placeholders.push(
+            <span className="item placeholder" key={index}></span>
+        ))
 
         return (
-            <div className={className} {...props}>
-                <h2>{data.name}</h2>
+            <ComponentContainer
+                className={className}
+                title={data.name}
+                {...props}
+            >
                 <div className="types">
                     {types.map((obj, index) => (
                         <ShipTypeTag
@@ -107,13 +131,15 @@ class ShipCollection extends React.Component {
                             key={index}
                         />
                     ))}
+                    {placeholders}
                 </div>
-                {!!(extraShips.length) && <div className="extra-ships">
-                    {extraShips.map((ship, index) => (
-                        <ShipExtra ship={ship} key={index} />
-                    ))}
-                </div>}
-            </div>
+                {!!(extraShips.length) &&
+                    <ListShips
+                        list={extraShips}
+                        navy={true}
+                    />
+                }
+            </ComponentContainer>
         )
     }
 }
@@ -124,20 +150,10 @@ class ShipTypeTag extends React.Component {
         return (
             <span className={classNames({
                 [this.props.className]: true,
+                'item': true,
                 'on': this.props.on
             })}>
-                {this.props.name}
-                <small className="code">[{this.props.code}]</small>
-            </span>
-        )
-    }
-}
-
-class ShipExtra extends React.Component {
-    render() {
-        return (
-            <span>
-                {this.props.ship._name}
+                {this.props.name} <small className="code">[{this.props.code}]</small>
             </span>
         )
     }
