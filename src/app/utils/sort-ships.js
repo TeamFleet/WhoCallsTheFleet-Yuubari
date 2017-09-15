d81850c2ef5ba75600c6fbc83f3e8ff675eb82f7
Diablohu <diablohu@users.noreply.github.com>
2017-09-16 05:01:45 +0800

    sort-ships

--- src/app/utils/sort-ships.js
+++ src/app/utils/sort-ships.js
@@ -1,13 +1,25 @@
 import getShip from '@appUtils/get-ship'
 
-export default (list = []) => list.sort((a, b) => (
-    0 - getShip(a).series - getShip(b).series
-)).sort((a, b) => {
-    a = getShip(a)
-    b = getShip(b)
-    if (a.series === b.series) return a.name.suffix - b.name.suffix
-    return a.order - b.order
-})
+export default (list = []) => list
+    // .sort((a, b) => {
+    //     a = getShip(a)
+    //     b = getShip(b)
+    //     // console.log(a.series, b.series)
+    //     if (a.series !== b.series) return 1
+    //     return 0
+    //     // return 0 - getShip(a).series - getShip(b).series
+    // })
+    .sort((a, b) => (
+        getShip(a).series !== getShip(b).series
+            ? 1
+            : 0
+    ))
+    .sort((a, b) => {
+        a = getShip(a)
+        b = getShip(b)
+        if (a.series === b.series) return a.name.suffix - b.name.suffix
+        return a.order - b.order
+    })
 
 // export default (list = []) => list.sort((a, b) => (
 //     getShip(a).order - getShip(b).order
