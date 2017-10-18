4aa051bb00eadb22c3f5682df32dc8d4dc666857
Diablohu <diablohu@users.noreply.github.com>
2017-10-18 22:10:33 +0800

    SERVER_DOMAIN

--- apps/app/config/site.js
+++ apps/app/config/site.js
@@ -1,4 +1,4 @@
 export const name = 'Who Calls The Fleet'
 export const origin = 'https://fleet.moe' // 网站域名
-export const domain = 'localhost' // 网站域名
+export const domain = process.env.SERVER_DOMAIN || 'localhost' // 网站域名
 export const fb_app_id = ''
