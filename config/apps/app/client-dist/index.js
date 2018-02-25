b2ebeeff41be7e5c0fa6f6095439ea69f6e828fd
Diablohu <diablohu@users.noreply.github.com>
2018-02-26 00:07:45 +0800

    update dependencies

--- config/apps/app/client-dist/index.js
+++ config/apps/app/client-dist/index.js
@@ -1,117 +1,134 @@
-// const fs = require('fs-extra')
+const fs = require('fs-extra')
 const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const semver = require('semver')
 
 const {
-    _app: pathApp,
+    // _app: pathApp,
+    nodeModules: pathNodeModules,
     _appOutput: pathOutput,
     _appName: pathNameSub,
-    ...dirs
+    // ...dirs
 } = require('../../../directories.js')
 const publicPath = `/${pathNameSub}/`
 
 const pluginCopyImages = require('../base/plugin-copy-images')
 
-const config = require('../base/factory')({
+const defaults = require('../base/factory')({
     isExtractTextPlugin: true
 })
 
 const isAnalyze = process.env.WEBPACK_ANALYZER || false
 
-module.exports = (async () => Object.assign({}, config, {
+module.exports = (async () => {
+    const webpackVersion = fs.readJsonSync(
+        path.resolve(pathNodeModules, './webpack/package.json')
+    ).version
 
-    analyzer: isAnalyze,
+    let config = {}
+    try {
+        await (async () => {
+            config = {
 
-    entry: {
-        ...config.entry,
-        commons: [
-            'react',
-            'react-dom',
+                analyzer: isAnalyze,
 
-            'redux',
-            'redux-thunk',
-            'react-redux',
+                entry: {
+                    ...defaults.entry,
+                    commons: [
+                        'react',
+                        'react-dom',
 
-            'react-router',
-            'react-router-redux',
+                        'redux',
+                        'redux-thunk',
+                        'react-redux',
 
-            'react-transition-group',
+                        'react-router',
+                        'react-router-redux',
 
-            // 'localforage',
-            'lz-string',
-            'metas',
-            'classnames',
-            'js-cookie',
+                        'react-transition-group',
 
-            'kckit',
-        ]
-    },
+                        // 'localforage',
+                        'lz-string',
+                        'metas',
+                        'classnames',
+                        'js-cookie',
 
-    output: {
-        // filename: `[name].[chunkhash].js`,
-        // chunkFilename: `chunk.[name].[chunkhash].js`,
-        filename: `core.[chunkhash].js`,
-        chunkFilename: `chunk.[chunkhash].js`,
-        path: pathOutput,
-        publicPath: publicPath // TODO 改成静态第三方URL用于CDN部署 http://localhost:3000/
-    },
-
-    plugins: [
-        'default',
-        {
-            'pwa': {
-                outputPath: path.resolve(pathOutput, '../'),
-                outputFilename: `service-worker.${pathNameSub}.js`,
-                outputFilenameHash: false,
-                // customServiceWorkerPath: path.normalize(appPath + '/src/client/custom-service-worker.js'),
-                globPattern: `/${pathNameSub}/**/*`,
-                globOptions: {
-                    ignore: [
-                        '/**/_*/',
-                        '/**/_*/**/*',
-                        '/**/chunk.database.*'
+                        'kckit',
                     ]
                 },
-                // appendUrls: getUrlsFromRouter()
-                appendUrls: []
-            }
-        },
-        [
-            ...config.plugins,
-            new ExtractTextPlugin('[name].[chunkhash].css'),
-            new webpack.DefinePlugin({
-                '__ELECTRON__': false,
-                '__PUBLIC__': JSON.stringify(publicPath),
-            }),
-            // new webpack.optimize.CommonsChunkPlugin({
-            //     children: true,
-            //     deepChildren: true,
-            // }),
-            new webpack.optimize.CommonsChunkPlugin({
-                names: [
-                    'commons',
-                    'critical',
-                ],
-                filename: 'core.[chunkhash].js'
-            }),
-            // new webpack.optimize.CommonsChunkPlugin({
-            //     name: "commons",
-            //     filename: '[name].[chunkhash].js',
-            //     minChunks: 3
-            // }),
-            ...(isAnalyze ? [] : await pluginCopyImages()),
-        ]
-    ],
 
-    // optimization: {
-    //     splitChunks: {
-    //         names: [
-    //             'commons',
-    //             'critical',
-    //         ],
-    //         filename: 'core.[chunkhash].js'
-    //     }
-    // }
-})
-)()
+                output: {
+                    // filename: `[name].[chunkhash].js`,
+                    // chunkFilename: `chunk.[name].[chunkhash].js`,
+                    filename: `core.[chunkhash].js`,
+                    chunkFilename: `chunk.[chunkhash].js`,
+                    path: pathOutput,
+                    publicPath: publicPath // TODO 改成静态第三方URL用于CDN部署 http://localhost:3000/
+                },
+
+                plugins: [
+                    'default',
+                    {
+                        'pwa': {
+                            outputPath: path.resolve(pathOutput, '../'),
+                            outputFilename: `service-worker.${pathNameSub}.js`,
+                            outputFilenameHash: false,
+                            // customServiceWorkerPath: path.normalize(appPath + '/src/client/custom-service-worker.js'),
+                            globPattern: `/${pathNameSub}/**/*`,
+                            globOptions: {
+                                ignore: [
+                                    '/**/_*/',
+                                    '/**/_*/**/*',
+                                    '/**/chunk.database.*'
+                                ]
+                            },
+                            // appendUrls: getUrlsFromRouter()
+                            appendUrls: []
+                        }
+                    },
+                    [
+                        ...defaults.plugins,
+                        new ExtractTextPlugin('[name].[chunkhash].css'),
+                        new webpack.DefinePlugin({
+                            '__ELECTRON__': false,
+                            '__PUBLIC__': JSON.stringify(publicPath),
+                        }),
+                        // new webpack.optimize.CommonsChunkPlugin({
+                        //     children: true,
+                        //     deepChildren: true,
+                        // }),
+                        // new webpack.optimize.CommonsChunkPlugin({
+                        //     name: "commons",
+                        //     filename: '[name].[chunkhash].js',
+                        //     minChunks: 3
+                        // }),
+                        ...(isAnalyze ? [] : await pluginCopyImages()),
+                    ]
+                ],
+            }
+        })()
+
+        const optimizationSplitChunks = {
+            names: [
+                'commons',
+                'critical',
+            ],
+            filename: 'core.[chunkhash].js'
+        }
+    
+        if (semver.satisfies(webpackVersion, '>= 4.0.0')) {
+            if (!config.optimization)
+                config.optimization = {}
+            config.optimization.splitChunks = optimizationSplitChunks
+        } else {
+            config.plugins[2].push(
+                new webpack.optimize.CommonsChunkPlugin(optimizationSplitChunks)
+            )
+        }
+    } catch (e) {
+        console.log(e)
+    }
+
+    return Object.assign({}, defaults, config)
+})()
