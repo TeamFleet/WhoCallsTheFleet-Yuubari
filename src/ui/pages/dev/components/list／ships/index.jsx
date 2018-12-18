cfa28bf737465cea1aa40adc490eda1ae514cb15
Diablohu <diablohu@users.noreply.github.com>
2018-12-19 01:56:11 +0800

    update dev

--- src/ui/pages/dev/components/list／ships/index.jsx
+++ src/ui/pages/dev/components/list／ships/index.jsx
@@ -1,12 +1,54 @@
 import React from 'react'
 import { extend } from 'koot'
 
+import Title from '@ui/components/title'
+import ListShips from '@ui/components/list/ships'
+const list = [394, 395, 396, 397, 398, 399, 400, 401, 402, 403, 404,]
+
 const DevListShips = extend({
 
 })(
     () => {
         return (
-            <div>list/ships123123</div>
+            <div>
+                <Title component="h2">list / ships</Title>
+                <ListShips
+                    list={list}
+                    empty="EMPTY"
+                />
+                <Title component="h4">unsort</Title>
+                <ListShips
+                    list={list}
+                    empty="EMPTY"
+                    sort={false}
+                />
+                <Title component="h4">without navy, with min-level</Title>
+                <ListShips
+                    list={list}
+                    empty="EMPTY"
+                    navy={false}
+                    min-level={true}
+                />
+                <Title component="h4">without type</Title>
+                <ListShips
+                    list={list}
+                    empty="EMPTY"
+                    type={false}
+                />
+                <Title component="h4">size: mini</Title>
+                <ListShips
+                    list={list}
+                    empty="EMPTY"
+                    size="mini"
+                />
+                <Title component="h4">size: mini, no grid</Title>
+                <ListShips
+                    list={list}
+                    empty="EMPTY"
+                    size="mini"
+                    grid={false}
+                />
+            </div>
         )
     }
 )
