fedbb644d9ce3fbff178e6baede047ffcc964c8f
Diablohu <Diablohu@users.noreply.github.com>
2019-05-10 16:23:09 +0800

    update linters

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    extends: ['koot'],
+    globals: {
+        __BGIMG_LIST__: 'readonly',
+        __CHANNEL__: 'readonly',
+        __ELECTRON__: 'readonly',
+        __ICONSVG__: 'readonly',
+        __PUBLIC__: 'readonly',
+        __SWIPER_CSS__: 'readonly',
+        __SVG_SYMBOLS__: 'readonly',
+        __webpack_public_path__: 'writable',
+        _hmt: 'readonly',
+        Nedb: 'readonly',
+        ga: 'readonly'
+    },
+    settings: {
+        'import/ignore': ['node_modules', 'dist', 'dist*']
+    }
+};
