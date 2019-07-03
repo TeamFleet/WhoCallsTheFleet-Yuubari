da4bdf2d8d24b692d8534a5594f9fcc085a7f6b7
Diablohu <diablohu@users.noreply.github.com>
2019-07-03 21:59:24 +0800

    update dependencies & polish

--- src/ui/components/improvement/index.jsx
+++ src/ui/components/improvement/index.jsx
@@ -1,108 +1,116 @@
-import React from 'react'
-import { extend } from 'koot'
+import React from 'react';
+import { extend } from 'koot';
 
-import Link from '@ui/components/link'
-import Stat from '@ui/components/stat'
-import LinkEquipment from '@ui/components/link/equipment'
+import Link from '@ui/components/link';
+import Stat from '@ui/components/stat';
+import LinkEquipment from '@ui/components/link/equipment';
 // import Star from './star'
 
-import getTimeJST from '@utils/get-time-jst'
-import getLink from '@utils/get-link'
-import getShip from '@utils/get-ship'
-import sortShips from '@utils/sort-ships'
-import arrResources from '@const/resources'
-import db from '@database'
+import _getValue from '@utils/get-value';
+import getTimeJST from '@utils/get-time-jst';
+import getLink from '@utils/get-link';
+import getShip from '@utils/get-ship';
+import sortShips from '@utils/sort-ships';
+import arrResources from '@const/resources';
+import db from '@database';
 
 const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thurday",
-    "Friday",
-    "Saturday"
-]
-
-
-//
-
-
-const Improvement = () => null
-export default Improvement
-
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thurday',
+    'Friday',
+    'Saturday'
+];
 
 //
 
+const Improvement = () => null;
+export default Improvement;
+
+//
 
 @extend({
     styles: require('./styles-dayandship.less')
 })
 class DayAndShip extends React.Component {
     renderItem(data, index) {
-        let [dataDays, dataShips] = data
+        let [dataDays, dataShips] = data;
         if (dataShips)
-            dataShips = sortShips(dataShips.map(shipId => getShip(shipId)))/*.sort((a, b) => a.order - b.order)*/
+            dataShips = sortShips(
+                dataShips.map(shipId => getShip(shipId))
+            ); /*.sort((a, b) => a.order - b.order)*/
         return (
             <div className="item" key={index}>
                 <div className="days">
                     {days.map((day, index) => (
-                        <span className={'day' + (dataDays[index] ? ' on' : '')} key={index}>{__(`day_abbr`, day)}</span>
+                        <span
+                            className={'day' + (dataDays[index] ? ' on' : '')}
+                            key={index}
+                        >
+                            {__(`day_abbr`, day)}
+                        </span>
                     ))}
                 </div>
-                <div className={"ships" + (!dataShips ? ' is-any' : '')}>
-                    {dataShips && dataShips.map((ship, index) => (
-                        <Link className="ship color-alt" key={index} to={getLink('ship', ship.id)}>
-                            {ship.getName(__('shipname_dash_none'))}
-                        </Link>
-                    ))}
+                <div className={'ships' + (!dataShips ? ' is-any' : '')}>
+                    {dataShips &&
+                        dataShips.map((ship, index) => (
+                            <Link
+                                className="ship color-alt"
+                                key={index}
+                                to={getLink('ship', ship.id)}
+                            >
+                                {ship.getName(__('shipname_dash_none'))}
+                            </Link>
+                        ))}
                     {!dataShips && __('improvement.any_2nd_ship')}
                 </div>
             </div>
-        )
+        );
     }
     render() {
-        let data = this.props.data || this.props.req || this.props.require
-        if (typeof data === 'object' && data.req)
-            data = data.req
-        if (!Array.isArray(data) || !data.length)
-            data = [
-                [[], false]
-            ]
+        let data = this.props.data || this.props.req || this.props.require;
+        if (typeof data === 'object' && data.req) data = data.req;
+        if (!Array.isArray(data) || !data.length) data = [[[], false]];
 
         return (
-            <div className={this.props.className} data-day={getTimeJST().getDay()}>
+            <div
+                className={this.props.className}
+                data-day={getTimeJST().getDay()}
+            >
                 {data.map(this.renderItem.bind(this))}
             </div>
-        )
+        );
     }
 }
-export { DayAndShip }
+export { DayAndShip };
 
-const isUndefined = value => (typeof value === 'undefined' || value === -1)
-import _getValue from '@utils/get-value'
+const isUndefined = value => typeof value === 'undefined' || value === -1;
 const getValue = value => {
-    if (isUndefined(value)) value = undefined
-    return _getValue(value)
-}
+    if (isUndefined(value)) value = undefined;
+    return _getValue(value);
+};
 
 class _Resources extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
 
-        this.data = props.data || props.resource || props.resources
-        this.upgradable = props.upgradable
+        this.data = props.data || props.resource || props.resources;
+        this.upgradable = props.upgradable;
 
         if (typeof this.data === 'object' && this.data.resource) {
-            if (typeof this.upgradable === 'undefined' &&
-                Array.isArray(this.data.upgrade) && this.data.upgrade.length
+            if (
+                typeof this.upgradable === 'undefined' &&
+                Array.isArray(this.data.upgrade) &&
+                this.data.upgrade.length
             )
-                this.upgradable = true
-            this.data = this.data.resource
+                this.upgradable = true;
+            this.data = this.data.resource;
         }
-        if (!Array.isArray(this.data))
-            this.data = []
+        if (!Array.isArray(this.data)) this.data = [];
 
-        if (this.data.length < 3) this.upgradable = false
+        if (this.data.length < 3) this.upgradable = false;
     }
 }
 
@@ -111,84 +119,99 @@ class _Resources extends React.Component {
 })
 class Resources extends _Resources {
     renderCategory(category, data) {
-        const title = typeof category === 'string'
-            ? __(`improvement`, category)
-            : (`★+${category} ~ ` + (category === 0 ? '+6' : 'MAX'))
-        let resources = null
-        let equipments = null
+        const title =
+            typeof category === 'string'
+                ? __(`improvement`, category)
+                : `★+${category} ~ ` + (category === 0 ? '+6' : 'MAX');
+        let resources = null;
+        let equipments = null;
 
         if (!data) {
-            resources = <span className="item disabled">-</span>
+            resources = <span className="item disabled">-</span>;
         } else {
             switch (category) {
                 case 'resources': {
                     resources = arrResources.map((resource, index) => {
-                        const value = getValue(data[index])
+                        const value = getValue(data[index]);
                         return (
                             <Stat
-                                className='item item-resource'
+                                className="item item-resource"
                                 key={resource}
                                 stat={resource}
                             >
                                 {value}
                             </Stat>
-                        )
-                    })
-                    break
+                        );
+                    });
+                    break;
                 }
                 default: {
                     resources = [
                         <Stat
-                            className='item item-resource'
-                            key='dev'
-                            stat='dev'
+                            className="item item-resource"
+                            key="dev"
+                            stat="dev"
                         >
                             {getValue(data[0])}
-                            <span className="guaranteed">({getValue(data[1])})</span>
+                            <span className="guaranteed">
+                                ({getValue(data[1])})
+                            </span>
                         </Stat>,
                         <Stat
-                            className='item item-resource'
-                            key='screw'
-                            stat='screw'
+                            className="item item-resource"
+                            key="screw"
+                            stat="screw"
                         >
                             {getValue(data[2])}
-                            <span className="guaranteed">({getValue(data[3])})</span>
+                            <span className="guaranteed">
+                                ({getValue(data[3])})
+                            </span>
                         </Stat>
-                    ]
+                    ];
                 }
             }
 
             // equipments
             const list = Array.isArray(data[4])
                 ? data[4].filter(item => item[1] !== 0)
-                : (data[4] ? [[data[4], data[5]]] : [])
+                : data[4]
+                ? [[data[4], data[5]]]
+                : [];
             if (list.length) {
-                equipments = (<span className="equipments">
-                    {list.map(arr => {
-                        if (isNaN(arr[0])) {
-                            const id = parseInt(arr[0].substr(arr[0].indexOf('_') + 1))
-                            return (
-                                <span
-                                    className="equipment equipment-other color-alt"
-                                    key={arr[0]}
-                                >
-                                    {db.consumables[id]._name}
-                                    <span className="guaranteed">x{getValue(arr[1])}</span>
-                                </span>
-                            )
-                        } else {
-                            return (
-                                <LinkEquipment
-                                    equipment={arr[0]}
-                                    className="equipment color-alt"
-                                    key={arr[0]}
-                                >
-                                    <span className="guaranteed">x{getValue(arr[1])}</span>
-                                </LinkEquipment>
-                            )
-                        }
-                    })}
-                </span>)
+                equipments = (
+                    <span className="equipments">
+                        {list.map(arr => {
+                            if (isNaN(arr[0])) {
+                                const id = parseInt(
+                                    arr[0].substr(arr[0].indexOf('_') + 1)
+                                );
+                                return (
+                                    <span
+                                        className="equipment equipment-other color-alt"
+                                        key={arr[0]}
+                                    >
+                                        {db.consumables[id]._name}
+                                        <span className="guaranteed">
+                                            x{getValue(arr[1])}
+                                        </span>
+                                    </span>
+                                );
+                            } else {
+                                return (
+                                    <LinkEquipment
+                                        equipment={arr[0]}
+                                        className="equipment color-alt"
+                                        key={arr[0]}
+                                    >
+                                        <span className="guaranteed">
+                                            x{getValue(arr[1])}
+                                        </span>
+                                    </LinkEquipment>
+                                );
+                            }
+                        })}
+                    </span>
+                );
             }
         }
 
@@ -198,21 +221,43 @@ class Resources extends _Resources {
                 {resources}
                 {equipments}
             </div>
-        )
+        );
     }
 
     render() {
         return (
             <div className={this.props.className}>
-                {this.renderCategory('resources', this.data[0] || [undefined, undefined, undefined, undefined])}
-                {this.renderCategory(0, this.data[1] || [undefined, undefined, undefined, undefined, undefined])}
-                {this.renderCategory(6, this.data[2] || [undefined, undefined, undefined, undefined, undefined])}
-                {this.upgradable && this.renderCategory('upgrading', this.data[3])}
+                {this.renderCategory(
+                    'resources',
+                    this.data[0] || [undefined, undefined, undefined, undefined]
+                )}
+                {this.renderCategory(
+                    0,
+                    this.data[1] || [
+                        undefined,
+                        undefined,
+                        undefined,
+                        undefined,
+                        undefined
+                    ]
+                )}
+                {this.renderCategory(
+                    6,
+                    this.data[2] || [
+                        undefined,
+                        undefined,
+                        undefined,
+                        undefined,
+                        undefined
+                    ]
+                )}
+                {this.upgradable &&
+                    this.renderCategory('upgrading', this.data[3])}
                 {!this.upgradable && this.renderCategory('upgrading', false)}
             </div>
-        )
+        );
     }
 }
-export { Resources }
+export { Resources };
 
-export { default as Star } from './star'
+export { default as Star } from './star';
--- src/ui/pages/equipment/details/components/improvements/index.jsx
+++ src/ui/pages/equipment/details/components/improvements/index.jsx
@@ -1,63 +1,69 @@
-import React from 'react'
-import { extend } from 'koot'
+import React from 'react';
+import { extend } from 'koot';
 
-import ComponentContainer from '@ui/containers/infos-component'
-import { placeholders } from '@ui/containers/flex'
-import { DayAndShip, Resources, Star } from '@ui/components/improvement'
-import Bullet from '@ui/components/bullet'
-import LinkEquipment from '@ui/components/link/equipment'
+import ComponentContainer from '@ui/containers/infos-component';
+import { placeholders } from '@ui/containers/flex';
+import { DayAndShip, Resources, Star } from '@ui/components/improvement';
+import Bullet from '@ui/components/bullet';
+import LinkEquipment from '@ui/components/link/equipment';
 
 const EquipmentDetailsComponentImprovements = extend({
     styles: require('./styles.less')
-})(
-    ({ className, equipment }) => {
-        const list = equipment.improvement || []
-        const hasItem = !!(list.length)
-        const upgradable = (Array.isArray(equipment.upgrade_to) && equipment.upgrade_to.length)
-        return (
-            <ComponentContainer className={className} title={__("equipment_details.improvements")}>
-                {hasItem && <div className="list">
+})(({ className, equipment }) => {
+    const list = equipment.improvement || [];
+    const hasItem = !!list.length;
+    const upgradable =
+        Array.isArray(equipment.upgrade_to) && equipment.upgrade_to.length;
+    return (
+        <ComponentContainer
+            className={className}
+            title={__('equipment_details.improvements')}
+        >
+            {hasItem && (
+                <div className="list">
                     {list.map((data, index) => (
                         <EquipmentDetailsComponentImprovementsImprovement
-                            data={data} key={index} upgradable={upgradable} className="flex-item" />
+                            data={data}
+                            key={index}
+                            upgradable={upgradable}
+                            className="flex-item"
+                        />
                     ))}
                     {list.length > 1 && placeholders}
-                </div>}
-                {!hasItem && <span className="disabled">{__("none")}</span>}
-            </ComponentContainer>
-        )
-    }
-)
-export default EquipmentDetailsComponentImprovements
+                </div>
+            )}
+            {!hasItem && <span className="disabled">{__('none')}</span>}
+        </ComponentContainer>
+    );
+});
+export default EquipmentDetailsComponentImprovements;
 
 const EquipmentDetailsComponentImprovementsImprovement = extend({
     styles: require('./styles-improvement.less')
-})(
-    ({
-        className, upgradable,
-        data: {
-            upgrade, req, resource
-        }
-    }) =>
-        <div className={className}>
-            <Bullet
-                className="upgradability"
-                level={upgrade ? 2 : 0}
-            >
-                <span className="subtitle">
-                    {upgrade ? __(`equipment_details.upgrade_to`) : __(`equipment_details.facts_unupgradable`)}
-                </span>
-                {upgrade && (
-                    <LinkEquipment
-                        equipment={upgrade[0]}
-                        className="equipment color-alt"
-                    >
-                        {upgrade && !!(upgrade[1]) && <Star className="default-star" star={upgrade[1]} />}
-                    </LinkEquipment>
-                )}
-            </Bullet>
-            <DayAndShip className="dayships" data={req} />
-            <Resources className="resources" data={resource} upgradable={upgradable} />
-        </div>
-
-)
+})(({ className, upgradable, data: { upgrade, req, resource } }) => (
+    <div className={className}>
+        <Bullet className="upgradability" level={upgrade ? 2 : 0}>
+            <span className="subtitle">
+                {upgrade
+                    ? __(`equipment_details.upgrade_to`)
+                    : __(`equipment_details.facts.unupgradable`)}
+            </span>
+            {upgrade && (
+                <LinkEquipment
+                    equipment={upgrade[0]}
+                    className="equipment color-alt"
+                >
+                    {upgrade && !!upgrade[1] && (
+                        <Star className="default-star" star={upgrade[1]} />
+                    )}
+                </LinkEquipment>
+            )}
+        </Bullet>
+        <DayAndShip className="dayships" data={req} />
+        <Resources
+            className="resources"
+            data={resource}
+            upgradable={upgradable}
+        />
+    </div>
+));
