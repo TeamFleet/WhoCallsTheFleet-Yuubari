a74cff4fa4d45f39f55a1b0c858872c5eddd81c8
Diablohu <Diablohu@users.noreply.github.com>
2019-07-31 17:22:40 +0800

    support /.well-known request

--- src/_server/lifecycle/before.js
+++ src/_server/lifecycle/before.js
@@ -1,26 +1,40 @@
-const path = require('path')
-const getDistPath = require('koot/utils/get-dist-path')
+const path = require('path');
+const getDistPath = require('koot/utils/get-dist-path');
 
-export default async (app) => {
+const Koa = require('koa');
+const mount = require('koa-mount');
+const koaStatic = require('koa-static');
 
+export default async app => {
     // 静态目录: 图片资源
     {
-        const Koa = require('koa')
-        const mount = require('koa-mount')
-        const koaStatic = require('koa-static')
-        const staticPicsServer = new Koa()
+        const staticPicsServer = new Koa();
         const dirPics = (() => {
             if (process.env.WEBPACK_BUILD_ENV === 'dev')
-                return path.resolve(process.cwd(), 'pics')
-            return path.resolve(getDistPath(), 'pics')
-        })()
-        staticPicsServer.use(koaStatic(dirPics, {
-            maxage: 30 * 24 * 60 * 60 * 1000,
-            hidden: false,
-            defer: false,
-            gzip: true,
-            extensions: false
-        }))
-        app.use(mount(`/pics`, staticPicsServer))
+                return path.resolve(process.cwd(), 'pics');
+            return path.resolve(getDistPath(), 'pics');
+        })();
+        staticPicsServer.use(
+            koaStatic(dirPics, {
+                maxage: 30 * 24 * 60 * 60 * 1000,
+                hidden: false,
+                defer: false,
+                gzip: true,
+                extensions: false
+            })
+        );
+        app.use(mount(`/pics`, staticPicsServer));
     }
-}
+
+    // /.well-known
+    {
+        const staticWellKnown = new Koa();
+        const dir = path.resolve(getDistPath(), '.well-known');
+        staticWellKnown.use(
+            koaStatic(dir, {
+                hidden: true
+            })
+        );
+        app.use(mount(`/.well-known`, staticWellKnown));
+    }
+};
