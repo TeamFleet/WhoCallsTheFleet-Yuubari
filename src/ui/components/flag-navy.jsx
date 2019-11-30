b716baadb0ac0dcbeda219b391461c75d44dc4ff
Diablohu <diablohu@users.noreply.github.com>
2019-12-01 03:08:45 +0800

    update database, pics & deps

--- src/ui/components/flag-navy.jsx
+++ src/ui/components/flag-navy.jsx
@@ -4,25 +4,28 @@ import { extend } from 'koot';
 
 const navies = [
     'ijn', // 日本帝国海军
-    'km', // 纳粹德国海军"
-    'rm', // 意大利皇家海军"
-    'mn', // 法国海军"
-    'rn', // 英国皇家海军"
-    'usn', // 美国海军"
-    'vmf', // 苏联海军"
-    'sm' // 瑞典海军"
+    'km', // 纳粹德国海军
+    'rm', // 意大利皇家海军
+    'mn', // 法国海军
+    'rn', // 英国皇家海军
+    'usn', // 美国海军
+    'vmf', // 苏联海军
+    'sm', // 瑞典海军
+    'ran', // 澳大利亚皇家海军
+    'rnln' // 荷兰皇家海军
 ];
 
 const getNavyIndex = navy => {
-    switch (navy) {
-        case 'ran': {
-            // 澳大利亚皇家海军"
-            return navies.indexOf('rn');
-        }
-        default: {
-            return navies.indexOf(navy);
-        }
-    }
+    return navies.indexOf(navy);
+    // switch (navy) {
+    //     case 'ran': {
+    //         // 澳大利亚皇家海军"
+    //         return navies.indexOf('rn');
+    //     }
+    //     default: {
+    //         return navies.indexOf(navy);
+    //     }
+    // }
 };
 
 const FlagNavy = extend({
