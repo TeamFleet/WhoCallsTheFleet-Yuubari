34beab30cd8d4d6bb68083f87712b2d3a1a0dae9
Diablohu <Diablohu@users.noreply.github.com>
2019-12-17 20:47:53 +0800

    update deps

--- src/ui/pages/home/index.jsx
+++ src/ui/pages/home/index.jsx
@@ -1,43 +1,36 @@
-import React from 'react'
-import { extend } from 'koot'
+import React from 'react';
+import { extend } from 'koot';
 
-import htmlHead from '@utils/html-head'
+import htmlHead from '@utils/html-head';
 
-import Page from '@ui/containers/page'
-import CenterContainer from '@ui/containers/center'
-import Markdown from '@ui/components/markdown'
+import Page from '@ui/containers/page';
+import CenterContainer from '@ui/containers/center';
+import Markdown from '@ui/components/markdown';
 
-const getMD = (localeId) => {
-
+const getMD = localeId => {
     // TODO: write a webpack plugin to include docs
     // DOC('updates/1.0.0')
 
-    if (localeId === 'en')
-        return require(`@docs/updates/1.0.0/en.md`).default
-    if (localeId === 'ja')
-        return require(`@docs/updates/1.0.0/ja.md`).default
-    return require(`@docs/updates/1.0.0/zh.md`).default
-}
+    if (localeId === 'en') return require(`@docs/updates/1.0.0/en.md`).default;
+    if (localeId === 'ja') return require(`@docs/updates/1.0.0/ja.md`).default;
+    return require(`@docs/updates/1.0.0/zh.md`).default;
+};
 
 const PageHome = extend({
     connect: state => ({
         localeId: state.localeId
     }),
-    pageinfo: (state) => htmlHead(state),
+    pageinfo: state => htmlHead(state),
     styles: require('./styles.less')
-})(
-    ({ className, localeId }) => (
-        <Page className={className}>
-            <CenterContainer>
-                <Markdown
-                    source={getMD(localeId)}
-                    childAfter={
-                        <span className="end-of-doc"></span>
-                    }
-                />
-            </CenterContainer>
-        </Page>
-    )
-)
+})(({ className, localeId }) => (
+    <Page className={className}>
+        <CenterContainer>
+            <Markdown
+                source={getMD(localeId)}
+                childAfter={<span className="end-of-doc"></span>}
+            />
+        </CenterContainer>
+    </Page>
+));
 
-export default PageHome
+export default PageHome;
